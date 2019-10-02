5714af2a760c61cf2099d20bda4858a87bc6b294
a.balashov <alex.handsomedevil@gmail.com>
2019-10-02 18:21:44 +0300

    how the fuck is this working?!?!

--- primitives.cc
+++ primitives.cc
@@ -62,120 +62,6 @@ void drawWireFrame(const SDL_Surface* surface, Vec4 v0, Vec4 v1, Vec4 v2, Vec3 c
     drawLine(surface, v2.x, v2.y, v0.x, v0.y, color);
 }
 
-void drawTriangleHalfSpace(RenderContext* context, Vertex v0, Vertex v1, Vertex v2, Shader& shader)
-{
-    float* zBuffer = context->rtargets.zBuffer;
-    SDL_Surface* surface = context->surface;
-       
-    //preserve depth of a polygon via keeping its z coordinate in clip-space
-    float z0Inv = 1.f / (float)v0.pos.w;
-    float z1Inv = 1.f / (float)v1.pos.w;
-    float z2Inv = 1.f / (float)v2.pos.w;
-    
-    v0.pos = perspectiveDivide(v0.pos) * viewportTransform;
-    v1.pos = perspectiveDivide(v1.pos) * viewportTransform;
-    v2.pos = perspectiveDivide(v2.pos) * viewportTransform;
-
-    const float triArea = computeArea(v0.pos.xyz, v1.pos.xyz, v2.pos.xyz);
-    if(triArea < 0)
-        return;
-    
-    shader.prepareInterpolants(v0, v1, v2, z0Inv, z1Inv, z2Inv, triArea);
-	//28.4 fixed format
-	int x0 = std::floor(16.f * v0.pos.x + 0.5f);
-	int x1 = std::floor(16.f * v1.pos.x + 0.5f);
-	int x2 = std::floor(16.f * v2.pos.x + 0.5f);
-	int y0 = std::floor(16.f * v0.pos.y + 0.5f);
-	int y1 = std::floor(16.f * v1.pos.y + 0.5f);
-	int y2 = std::floor(16.f * v2.pos.y + 0.5f);
-
-    //compute triangle bounding box
-    int topY   = min((max(max(y0, y1), y2)) >> 4, context->window.height - 1);
-    int leftX  = max((min(min(x0, x1), x2)) >> 4, 0);
-    int botY   = max((min(min(y0, y1), y2)) >> 4, 0);
-    int rightX = min((max(max(x0, x1), x2)) >> 4, context->window.width - 1);
-
-    //calculate row and column step in barycentric coordinates
-    int A01 = y0 - y1;
-    int B01 = x1 - x0;
-
-    int A12 = y1 - y2;
-    int B12 = x2 - x1;
-
-    int A20 = y2 - y0;
-    int B20 = x0 - x2;
-
-    int FA01 = int(unsigned(A01) << 4);
-    int FB01 = int(unsigned(B01) << 4);
-    int FA12 = int(unsigned(A12) << 4);
-    int FB12 = int(unsigned(B12) << 4);
-    int FA20 = int(unsigned(A20) << 4);
-    int FB20 = int(unsigned(B20) << 4);
-	
-    float Z1Z0Inv = (z1Inv - z0Inv) / triArea;
-    float Z2Z0Inv = (z2Inv - z0Inv) / triArea;
-	
-	int fleftX = leftX << 4;
-	int ftopY =  topY << 4;
-
-    int w0StartRow = (x2 - x1) * (ftopY - y1) - (fleftX - x1) * (y2 - y1);
-	int w1StartRow = (x0 - x2) * (ftopY - y2) - (fleftX - x2) * (y0 - y2);
-	int w2StartRow = (x1 - x0) * (ftopY - y0) - (fleftX - x0) * (y1 - y0);
-
-	auto isTopLeft = [](int x1, int y1, int x2, int y2) {
-		//left/top edge
-		return (y1 == y2 && ((x1 - x2) > 0) ) || ((y2 - y1) < 0);
-	};
-
-	//fill rule
-	w0StartRow = isTopLeft(x1,y1,x2,y2) ? ++w0StartRow : w0StartRow;
-	w1StartRow = isTopLeft(x2,y2,x0,y0) ? ++w1StartRow : w1StartRow;
-	w2StartRow = isTopLeft(x0,y0,x1,y1) ? ++w2StartRow : w2StartRow;
-
-    bool discardFragment = false;
-    for(int y = topY; y > botY; y--) {
-
-        int w0 = w0StartRow;
-        int w1 = w1StartRow;
-        int w2 = w2StartRow;
-
-        for(int x = leftX; x <= rightX; x++) {
-			
-            if(w0>0 && w1>0 && w2>0) {
-                float Z = z0Inv + (w1/256.f) * Z1Z0Inv + (w2/256.f) * Z2Z0Inv;
-                Z = 1.f / Z;
-                if( Z < zBuffer[y * surface->w + x]) {
-                    zBuffer[y * surface->w + x] = Z;
-                    Vec3 gl_pixelCoord = {w1/256.f, w2/256.f, Z};
-                    discardFragment = false;
-                    Vec3 finalColor = shader.fragmentShader(gl_pixelCoord, discardFragment);
-                    if(!discardFragment)
-                        drawPixel(surface, x, y, finalColor);
-                }
-            }
-			
-            w0 += FA12;
-            w1 += FA20;
-            w2 += FA01;
-        }
-
-        w0StartRow -= FB12;
-        w1StartRow -= FB20;
-        w2StartRow -= FB01;
-    }
-}
-
-
-enum CoverageMaskFlagBits
-{
-	COVERAGE_NONE          = 0 << 0,
-    COVERAGE_RIGHT_TOP     = 1 << 0,
-    COVERAGE_LEFT_TOP      = 1 << 1,
-    COVERAGE_LEFT_BOTTOM   = 1 << 2,
-    COVERAGE_RIGHT_BOTTOM  = 1 << 3,
-    COVERAGE_FULL          = 0xf
-};
-
 struct SampleRastInfo
 {
 	float w0StartRow;
@@ -196,14 +82,14 @@ struct SampleRastInfo
 SampleRastInfo prepareSample(RenderContext* context, Vec3 v0, Vec3 v1, Vec3 v2, int sX, int sY)
 {
 	SampleRastInfo info = {};
-	printf("%d %d\n",sX,sY);
+	//printf("%d %d\n",sX,sY);
 	//28.4 fixed format
-	int x0 = std::floor(16.f * v0.x + 0.5f) + sX;
-	int x1 = std::floor(16.f * v1.x + 0.5f) + sX;
-	int x2 = std::floor(16.f * v2.x + 0.5f) + sX;
-	int y0 = std::floor(16.f * v0.y + 0.5f) + sY;
-	int y1 = std::floor(16.f * v1.y + 0.5f) + sY;
-	int y2 = std::floor(16.f * v2.y + 0.5f) + sY;
+	int x0 = std::floor(16.f * v0.x + 0.5f);// + sX;
+	int x1 = std::floor(16.f * v1.x + 0.5f);// + sX;
+	int x2 = std::floor(16.f * v2.x + 0.5f);// + sX;
+	int y0 = std::floor(16.f * v0.y + 0.5f);// + sY;
+	int y1 = std::floor(16.f * v1.y + 0.5f);// + sY;
+	int y2 = std::floor(16.f * v2.y + 0.5f);// + sY;
 
     //compute triangle bounding box
     int topY   = min((int)((max(max(y0, y1), y2))/16.f), context->window.height - 1);
@@ -228,8 +114,8 @@ SampleRastInfo prepareSample(RenderContext* context, Vec3 v0, Vec3 v1, Vec3 v2, 
     int FA20 = int(unsigned(A20) << 4);
     int FB20 = int(unsigned(B20) << 4);
 
-	int fleftX = leftX << 4;
-	int ftopY =  topY << 4;
+	int fleftX = (leftX << 4) + sX;
+	int ftopY =  (topY << 4) + sY;
 
     int w0StartRow = (x2 - x1) * (ftopY - y1) - (fleftX - x1) * (y2 - y1);
 	int w1StartRow = (x0 - x2) * (ftopY - y2) - (fleftX - x2) * (y0 - y2);
@@ -244,7 +130,6 @@ SampleRastInfo prepareSample(RenderContext* context, Vec3 v0, Vec3 v1, Vec3 v2, 
 	w0StartRow = isTopLeft(x1,y1,x2,y2) ? ++w0StartRow : w0StartRow;
 	w1StartRow = isTopLeft(x2,y2,x0,y0) ? ++w1StartRow : w1StartRow;
 	w2StartRow = isTopLeft(x0,y0,x1,y1) ? ++w2StartRow : w2StartRow;
-
 
 	info.w0StartRow = w0StartRow;
 	info.w1StartRow = w1StartRow;
@@ -262,6 +147,74 @@ SampleRastInfo prepareSample(RenderContext* context, Vec3 v0, Vec3 v1, Vec3 v2, 
 
 	return info;
 }
+
+void drawTriangleHalfSpace(RenderContext* context, Vertex v0, Vertex v1, Vertex v2, Shader& shader)
+{
+    float* zBuffer = context->rtargets.zBuffer;
+    SDL_Surface* surface = context->surface;
+       
+    //preserve depth of a polygon via keeping its z coordinate in clip-space
+    float z0Inv = 1.f / (float)v0.pos.w;
+    float z1Inv = 1.f / (float)v1.pos.w;
+    float z2Inv = 1.f / (float)v2.pos.w;
+    
+    v0.pos = perspectiveDivide(v0.pos) * viewportTransform;
+    v1.pos = perspectiveDivide(v1.pos) * viewportTransform;
+    v2.pos = perspectiveDivide(v2.pos) * viewportTransform;
+
+    const float triArea = computeArea(v0.pos.xyz, v1.pos.xyz, v2.pos.xyz);
+    if(triArea < 0)
+        return;
+    shader.prepareInterpolants(v0, v1, v2, z0Inv, z1Inv, z2Inv, triArea);
+
+    float Z1Z0Inv = (z1Inv - z0Inv) / triArea;
+    float Z2Z0Inv = (z2Inv - z0Inv) / triArea;
+
+	SampleRastInfo s = prepareSample(context, v0.pos.xyz, v1.pos.xyz, v2.pos.xyz, 0, 0);
+
+    bool discardFragment = false;
+    for(int y = s.topY; y > s.botY; y--) {
+
+        int w0 = s.w0StartRow;
+        int w1 = s.w1StartRow;
+        int w2 = s.w2StartRow;
+
+        for(int x = s.leftX; x <= s.rightX; x++) {
+			
+            if(w0>0 && w1>0 && w2>0) {
+                float Z = z0Inv + (w1/256.f) * Z1Z0Inv + (w2/256.f) * Z2Z0Inv;
+                Z = 1.f / Z;
+                if( Z < zBuffer[y * surface->w + x]) {
+                    zBuffer[y * surface->w + x] = Z;
+                    Vec3 gl_pixelCoord = {w1/256.f, w2/256.f, Z};
+                    discardFragment = false;
+                    Vec3 finalColor = shader.fragmentShader(gl_pixelCoord, discardFragment);
+                    if(!discardFragment)
+                        drawPixel(surface, x, y, finalColor);
+                }
+            }
+			
+            w0 += s.FA12;
+            w1 += s.FA20;
+            w2 += s.FA01;
+        }
+
+        s.w0StartRow -= s.FB12;
+        s.w1StartRow -= s.FB20;
+        s.w2StartRow -= s.FB01;
+    }
+}
+
+
+enum CoverageMaskFlagBits
+{
+	COVERAGE_NONE          = 0 << 0,
+    COVERAGE_RIGHT_TOP     = 1 << 0,
+    COVERAGE_LEFT_TOP      = 1 << 1,
+    COVERAGE_LEFT_BOTTOM   = 1 << 2,
+    COVERAGE_RIGHT_BOTTOM  = 1 << 3,
+    COVERAGE_FULL          = 0xf
+};
 
 void drawTriangleHalfSpaceMSAA(RenderContext* context, Vertex v0, Vertex v1, Vertex v2, Shader& shader)
 {
@@ -284,61 +237,26 @@ void drawTriangleHalfSpaceMSAA(RenderContext* context, Vertex v0, Vertex v1, Ver
         return;
     
     shader.prepareInterpolants(v0, v1, v2, z0Inv, z1Inv, z2Inv, triArea);
-	//28.4 fixed format
-	int x0 = std::floor(16.f * v0.pos.x + 0.5f);
-	int x1 = std::floor(16.f * v1.pos.x + 0.5f);
-	int x2 = std::floor(16.f * v2.pos.x + 0.5f);
-	int y0 = std::floor(16.f * v0.pos.y + 0.5f);
-	int y1 = std::floor(16.f * v1.pos.y + 0.5f);
-	int y2 = std::floor(16.f * v2.pos.y + 0.5f);
 
-    //compute triangle bounding box
-    int topY   = min((max(max(y0, y1), y2)) >> 4, context->window.height - 1);
-    int leftX  = max((min(min(x0, x1), x2)) >> 4, 0);
-    int botY   = max((min(min(y0, y1), y2)) >> 4, 0);
-    int rightX = min((max(max(x0, x1), x2)) >> 4, context->window.width - 1);
-
-    //calculate row and column step in barycentric coordinates
-    int A01 = y0 - y1;
-    int B01 = x1 - x0;
-
-    int A12 = y1 - y2;
-    int B12 = x2 - x1;
-
-    int A20 = y2 - y0;
-    int B20 = x0 - x2;
-
-    int FA01 = int(unsigned(A01) << 4);
-    int FB01 = int(unsigned(B01) << 4);
-    int FA12 = int(unsigned(A12) << 4);
-    int FB12 = int(unsigned(B12) << 4);
-    int FA20 = int(unsigned(A20) << 4);
-    int FB20 = int(unsigned(B20) << 4);
-	
     float Z1Z0Inv = (z1Inv - z0Inv) / triArea;
     float Z2Z0Inv = (z2Inv - z0Inv) / triArea;
 	
-	int fleftX = leftX << 4;
-	int ftopY =  topY << 4;
-
-    int w0StartRow = (x2 - x1) * (ftopY - y1) - (fleftX - x1) * (y2 - y1);
-	int w1StartRow = (x0 - x2) * (ftopY - y2) - (fleftX - x2) * (y0 - y2);
-	int w2StartRow = (x1 - x0) * (ftopY - y0) - (fleftX - x0) * (y1 - y0);
-
     bool discardFragment = false;
-
-	SampleRastInfo s1 = prepareSample(context, v0.pos.xyz, v1.pos.xyz, v2.pos.xyz, sampleLocX[0], sampleLocY[0]);
-	SampleRastInfo s2 = prepareSample(context, v0.pos.xyz, v1.pos.xyz, v2.pos.xyz, sampleLocX[1], sampleLocY[1]);
-	SampleRastInfo s3 = prepareSample(context, v0.pos.xyz, v1.pos.xyz, v2.pos.xyz, sampleLocX[2], sampleLocY[2]);
-	SampleRastInfo s4 = prepareSample(context, v0.pos.xyz, v1.pos.xyz, v2.pos.xyz, sampleLocX[3], sampleLocY[3]);
+	//printf("TRIANGLE COORDS ARE\n V0: %f %f %f\n V1: %f %f %f\n V2: %f %f %f\n",
+	//v0.pos.x,v0.pos.y,v0.pos.z,v1.pos.x,v1.pos.y,v1.pos.z,v2.pos.x,v2.pos.y,v2.pos.z);
+	SampleRastInfo s  = prepareSample(context, v0.pos.xyz, v1.pos.xyz, v2.pos.xyz, 8, 8);//8 is the offset to the pixel center
+	SampleRastInfo s1 = prepareSample(context, v0.pos.xyz, v1.pos.xyz, v2.pos.xyz, sampleLocX[0] + 8, sampleLocY[0] + 8);
+	SampleRastInfo s2 = prepareSample(context, v0.pos.xyz, v1.pos.xyz, v2.pos.xyz, sampleLocX[1] + 8, sampleLocY[1] + 8);
+	SampleRastInfo s3 = prepareSample(context, v0.pos.xyz, v1.pos.xyz, v2.pos.xyz, sampleLocX[2] + 8, sampleLocY[2] + 8);
+	SampleRastInfo s4 = prepareSample(context, v0.pos.xyz, v1.pos.xyz, v2.pos.xyz, sampleLocX[3] + 8, sampleLocY[3] + 8);
     int stride = 4;
 	assert(s2.topY >= s4.botY);
 	assert(s3.leftX <= s1.rightX);
     for(int y = s2.topY; y >= s4.botY; y--) {
 
-        int w0 = w0StartRow;
-        int w1 = w1StartRow;
-        int w2 = w2StartRow;
+        int w0 = s.w0StartRow;
+        int w1 = s.w1StartRow;
+        int w2 = s.w2StartRow;
 
         int w0s1 = s1.w0StartRow;
         int w1s1 = s1.w1StartRow;
@@ -394,7 +312,6 @@ void drawTriangleHalfSpaceMSAA(RenderContext* context, Vertex v0, Vertex v1, Ver
             }
 
             if(coverageMask) {  
-
                 float Z = z0Inv + (w1 >> 8) * Z1Z0Inv + (w2 >> 8) * Z2Z0Inv;
                 Z = 1.f / Z;
                 Vec3 gl_pixelCoord = {w1>>8, w2>>8, Z};
@@ -422,9 +339,9 @@ void drawTriangleHalfSpaceMSAA(RenderContext* context, Vertex v0, Vertex v1, Ver
 
             }
 			
-            w0 += FA12;
-            w1 += FA20;
-            w2 += FA01;
+            w0 += s.FA12;
+            w1 += s.FA20;
+            w2 += s.FA01;
 
             w0s1 += s1.FA12;
             w1s1 += s1.FA20;
@@ -443,9 +360,9 @@ void drawTriangleHalfSpaceMSAA(RenderContext* context, Vertex v0, Vertex v1, Ver
             w2s4 += s4.FA01;
         }
 
-        w0StartRow -= FB12;
-        w1StartRow -= FB20;
-        w2StartRow -= FB01;
+        s.w0StartRow -= s.FB12;
+        s.w1StartRow -= s.FB20;
+        s.w2StartRow -= s.FB01;
 
         s1.w0StartRow -= s1.FB12;
         s1.w1StartRow -= s1.FB20;
@@ -462,6 +379,5 @@ void drawTriangleHalfSpaceMSAA(RenderContext* context, Vertex v0, Vertex v1, Ver
         s4.w0StartRow -= s4.FB12;
         s4.w1StartRow -= s4.FB20;
         s4.w2StartRow -= s4.FB01;
-
     }
 }
